cbf7c5b2b6b5af13e2f4b752acf49a1a11f0607d
KocproZ <kocproz@protonmail.com>
2017-10-27 22:03:37 +0200

    Small fixes and some TODO's.

--- src/main/java/me/matrix89/markpages/data/repository/PageRepository.java
+++ src/main/java/me/matrix89/markpages/data/repository/PageRepository.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public interface PageRepository extends JpaRepository<PageModel, Long> {
     PageModel findAllByStringId(String stringId);
 
-//    List<PageModel> findAllByMaintainer_Id(Long maintainer_id);
+    //    List<PageModel> findAllByMaintainer_Id(Long maintainer_id); //TODO write a replacement
     List<PageModel> findAllByVisibility(PageModel.Visibility visibility, Sort sort);
     List<PageModel> findAllByVisibilityNot(PageModel.Visibility visibility, Sort sort);
 
