f5f7e2817a775770244ff55169391619f4a79b43
KocproZ <kocproz@protonmail.com>
2017-12-07 22:22:14 +0100

    .

--- src/main/resources/static/js/showdownExtensions.js
+++ src/main/resources/static/js/showdownExtensions.js
@@ -2,7 +2,7 @@ showdown.extension('audio', {
     type: 'lang',
     regex: /\?\[.+\]\((.+(?=\)))\)/g,
     replace: function (a, b, c) {
-        var element = '<audio controls="true" preload="auto">' +
+        var element = '<audio controls="true" preload="metadata">' +
             '<source src="' + b + '"/>' +
             '</audio>'
         return element;
