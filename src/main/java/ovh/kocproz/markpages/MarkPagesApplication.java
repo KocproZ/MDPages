37e179d71f48adf8e89e291c1f26cfcfff1e6763
KocproZ <kocproz@protonmail.com>
2017-12-07 07:59:49 +0100

    @PropertySource

--- src/main/java/ovh/kocproz/markpages/MarkPagesApplication.java
+++ src/main/java/ovh/kocproz/markpages/MarkPagesApplication.java
@@ -5,24 +5,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
+@PropertySource("application.properties")
 @SpringBootApplication
 @EnableScheduling
 public class MarkPagesApplication {
 
-	@Bean
-	public Logger logger() {
-		return LoggerFactory.getLogger(MarkPagesApplication.class.getName());
-	}
+    @Bean
+    public Logger logger() {
+        return LoggerFactory.getLogger(MarkPagesApplication.class.getName());
+    }
 
-	@Bean
-	public Pbkdf2PasswordEncoder passwordEncoder() {
-		return new Pbkdf2PasswordEncoder();
-	}
+    @Bean
+    public Pbkdf2PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarkPagesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarkPagesApplication.class, args);
+    }
 }
