70c52c8edcb9a31bbf2d01a157b7e15622c5e05b
Kuboczoch <jakub.pokorski@protonmail.com>
2019-09-06 05:18:51 +0200

    Footer and mixins with variable rethink

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import './style.scss';
+import Logo from "../logo";
 
 class Footer extends Component {
     render() {
         return (
             <div className={"footer"}>
-
+                <div className={"row"}>
+                    <div className={"col-8"}>
+                        <div className={"column"}>
+                            <ul>
+                                <li>MDPages</li>
+                                <li><a href={"https://github.com/KocproZ/MDPages"}>About</a></li>
+                                <li><a href={"https://github.com/KocproZ/MDPages"}>Docs</a></li>
+                                <li><a href={"https://github.com/KocproZ/MDPages"}>GitHub</a></li>
+                            </ul>
+                            <ul>
+                                <li>For</li>
+                                <li>Teams</li>
+                                <li>Education</li>
+                                <li>Writing</li>
+                                <li>Documentation</li>
+                            </ul>
+                            <ul>
+                                <li>Links</li>
+                                <li><a href={"https://github.com/Starchasers"}>Starchasers</a></li>
+                                <li><a href={"https://github.com/KocproZ/MDPages/issues"}>Issues</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={"col-4 center"}>
+                        <Logo/>
+                    </div>
+                </div>
             </div>
         );
     }
