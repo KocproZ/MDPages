9c26d48150845432e05308e87b13ec2f0cd24292
Kuboczoch <jakub.pokorski@protonmail.com>
2019-09-09 05:19:13 +0200

    Navigation width listener

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -5,8 +5,33 @@ import Logo from "../logo";
 import MobileButton from "./mobile-button";
 import {connect} from "react-redux";
 import {getNavigationVisibility} from "../../redux/selectors";
+import {toggleNavigation} from "../../redux/actions";
 
 class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {width: 0, height: 0};
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        this.setState({width: window.innerWidth, height: window.innerHeight});
+        if (this.state.width < 768 && this.props.visibility === true) {
+            this.props.dispatch(toggleNavigation())
+        }
+        else if(this.state.width > 768 && this.props.visibility === false){
+            this.props.dispatch(toggleNavigation())
+        }
+    }
 
     render() {
         return (
