52a20a54912365d9e39a3c8cecfbe7732b0881dc
Kuboczoch <jakub.pokorski@protonmail.com>
2019-09-10 04:58:36 +0200

    React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import Navigation from "./components/navigation";
 import Container from "./components/container";
+import {BrowserRouter as Router, Switch} from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Navigation/>
-                <Container/>
+                <Router>
+                    <Switch>
+                        <Container/>
+                    </Switch>
+                </Router>
             </div>
         );
     }
