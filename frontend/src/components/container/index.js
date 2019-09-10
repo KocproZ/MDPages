fdd50ffaba23346cbfdd333baafaebaf189f258f
Kuboczoch <jakub.pokorski@protonmail.com>
2019-09-10 04:58:52 +0200

    Home component

--- frontend/src/components/container/index.js
+++ frontend/src/components/container/index.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import './style.scss';
 import Footer from "../footer";
+import Home from "../sites/home/home";
+import {Route} from "react-router-dom";
 
 class Container extends Component {
     render() {
         return (
             <div className={"app"}>
                 <div className={"container"}>
-
+                    <Route exact path="/" component={Home}/>
                 </div>
                 <Footer/>
             </div>
---
+++ frontend/src/components/sites/home/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './style.scss';
+
+class Home extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div>KUPA</div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Home;
