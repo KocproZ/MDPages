40def7afdff4e7aa106d2be7bb0760c73c1667a9
Kuboczoch <jakub.pokorski@protonmail.com>
2019-10-16 01:04:05 +0200

    First little design #18

--- frontend/src/components/sites/home/home.js
+++ frontend/src/components/sites/home/home.js
@@ -5,16 +5,22 @@ class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className={"row"}>
-                    <div className={"col-12 col-md-6 homepage header"}>
-                    <span>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </span>
-                        <span>
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </span>
+                <div className={"row homepage"}>
+                    <div className={"col-12 col-md-8 col-xl-12 header"}>Latest news</div>
+                    <div className={"col-12 col-md-8 col-xl-6"}>
+                        <div className={"box"}>
+                            <h1>Hello!</h1>
+                            <p>Welcome to MDPages, this project is currently under some serious construction.</p>
+                            <p>Nowadays we are rewriting frontend and backend, many features may not work. But over time it will be awesome project!</p>
+                        </div>
                     </div>
-                    <div className={"col-12 col-md-6"}></div>
+                    <div className={"col-12 col-md-8 col-xl-6"}>
+                        <div className={"box"}>
+                            <h1>Hello!</h1>
+                            <p>Welcome to MDPages, this project is currently under some serious construction.</p>
+                            <p>Nowadays we are rewriting frontend and backend, many features may not work. But over time it will be awesome project!</p>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
